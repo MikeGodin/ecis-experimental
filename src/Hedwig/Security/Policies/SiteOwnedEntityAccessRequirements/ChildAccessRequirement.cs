86f7e390fb7d7d87b1989ad2d7301503caf1e811
julia <julia@skylight.digital>
2019-10-31 15:53:10 -0400

    Fixup child access requirement

--- src/Hedwig/Security/Policies/SiteOwnedEntityAccessRequirements/ChildAccessRequirement.cs
+++ src/Hedwig/Security/Policies/SiteOwnedEntityAccessRequirements/ChildAccessRequirement.cs
@@ -5,21 +5,23 @@ namespace Hedwig.Security
 {
     public class ChildAccessRequirement : IAuthorizationRequirement
     {
-        public Task Authorize(AuthorizationContext context)
+        public async Task Authorize(AuthorizationContext context)
         {
             var childIdString = (string) context.Arguments.ValueFor("id")?.Value;
             var userIdString = (string) context.User.FindFirst("sub")?.Value;
 
-            if (childIdString == null || userIdString == null) {
-                return Task.CompletedTask;
-            }
+            // No id or userId present --> nothing to check
+            if (childIdString == null || userIdString == null) return;
 
 
             var childId = Guid.Parse(childIdString);
             var userId = Int32.Parse(userIdString);
 
+            // check user can access child
             var permissionsHelper = RequestContextAccessor.GetRequestContext(context).PermissionsHelper;
-            return permissionsHelper.UserCanAccessChild(childId, userId);
+            if(! await permissionsHelper.UserCanAccessChild(childId, userId)) {
+                context.ReportError(ErrorMessages.USER_CANNOT_ACCESS_ENTITY("Child"));
+            }
         }
     }
 }
